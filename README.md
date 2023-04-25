b81ed7295228c89d04d215c33f4663c9b39cac45
Etudiant O'clock <tafrihipousser@gmail.com>
2023-04-25 13:49:20 +0200

    ajout lien exo 5

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pasUnChallenge---trainning-react
-Juste un petit entraînement pour réviser quelques notions de Reacts.
+Juste un petit entraînement pour réviser quelques notions de React.
--- src/components/Exo5/index.tsx
+++ src/components/Exo5/index.tsx
@@ -6,6 +6,9 @@ function Exo5() {
       <h2>
         Exo5 : Duppliquer tout le projet et tout faire fonctionner
         à nouveau avec Redux sans useState
+        {
+          // voir repo github https://github.com/helene-tafrihi-pousset/exercice-redux.git
+        }
       </h2>
     </div>
   );
